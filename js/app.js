135944da84d9f2093ef3fd3e2058fd79bcbc1c56
vebjorrs <vebjorrs@ifi.uio.no>
2014-12-08 19:22:59 +0100

    Made the 404-page be the default one, assigning the main page to \'/\' and \'/main\'.

--- js/app.js
+++ js/app.js
@@ -6,8 +6,10 @@ angular.module('myApp', [
 	'ngResource',
 ]).
 config(['$routeProvider', function($routeProvider, RestangularProvider) {
-	$routeProvider.when('/main', {templateUrl: 'partials/main.html', controller: 'MainCtrl'});
+	$routeProvider.when('/', {templateUrl: 'partials/main.html', controller: 'MainCtrl'});
+	$routeProvider.when('/main', {redirectTo: '/'});
 	$routeProvider.when('/flashcards', {templateUrl: 'partials/flashcards.html', controller: 'FlashCtrl'});
+	$routeProvider.when('/404', {templateUrl: 'partials/error404.html', controller: 'MainCtrl'});
 
-	$routeProvider.otherwise({redirectTo: '/main'});
+	$routeProvider.otherwise({redirectTo: '/404'});
 }]);
