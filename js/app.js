1b3a52293bc80b7c6b1e37c16ccae3bc68e25c24
vebjorrs <vebjorrs@ifi.uio.no>
2014-12-16 11:08:03 +0100

    Added support for HTML5 mode.

--- js/app.js
+++ js/app.js
@@ -7,11 +7,14 @@ angular.module('myApp', [
 	'ui.bootstrap.modal',
 	'ngResource'
 ]).
-config(['$routeProvider', function($routeProvider, RestangularProvider) {
+config(function($routeProvider, $locationProvider, RestangularProvider) {
+
 	$routeProvider.when('/', {templateUrl: 'partials/main.html', controller: 'MainCtrl'});
 	$routeProvider.when('/main', {redirectTo: '/'});
 	$routeProvider.when('/flashcards', {templateUrl: 'partials/flashcards.html', controller: 'FlashCtrl'});
 	$routeProvider.when('/404', {templateUrl: 'partials/error404.html', controller: 'MainCtrl'});
 
 	$routeProvider.otherwise({redirectTo: '/404'});
+
+	$locationProvider.html5Mode(true);
 }]);
