79aee5750613ddd86332664e229f66e875f4ac49
vebjorrs <vebjorrs@ifi.uio.no>
2014-12-16 11:36:30 +0100

    Removed unnecessary whitespace.

--- js/app.js
+++ js/app.js
@@ -8,12 +8,10 @@ angular.module('myApp', [
 	'ngResource'
 ]).
 config(function($routeProvider, $locationProvider) {
-
 	$routeProvider.when('/', {templateUrl: 'partials/main.html', controller: 'MainCtrl'});
 	$routeProvider.when('/main', {redirectTo: '/'});
 	$routeProvider.when('/flashcards', {templateUrl: 'partials/flashcards.html', controller: 'FlashCtrl'});
 	$routeProvider.when('/404', {templateUrl: 'partials/error404.html', controller: 'MainCtrl'});
-
 	$routeProvider.otherwise({redirectTo: '/404'});
 
 	$locationProvider.html5Mode(true);
